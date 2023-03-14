0295e2727056ea1946e8fa124f49c1c414cd1c8b
noseung-in <dmsgusaus33@naver.com>
2023-03-14 12:07:31 +0900

    시하누나 작업내용 반영 2023/03/14_senoh

--- MBTI_TeamProject/src/main/java/team/spring/springmbti/user/controller/MyPageController.java
+++ MBTI_TeamProject/src/main/java/team/spring/springmbti/user/controller/MyPageController.java
@@ -1,5 +1,10 @@
 package team.spring.springmbti.user.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -10,9 +15,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import team.spring.springmbti.character.service.CharacterService;
 import team.spring.springmbti.character.vo.CharacterInfo;
@@ -23,79 +31,92 @@ import team.spring.springmbti.user.vo.User;
 @SessionAttributes(value= { "myCharacter","myUser","battleCharacter","battleUser" })
 @RequestMapping(value = "mypage")
 public class MyPageController {
-	
-	Logger log = LogManager.getLogger("case3");
-	
-	@Autowired
-	private UserService service;
-	
-	@Autowired
-	private CharacterService cService;
-	
-	@ModelAttribute("myCharacter")
-	public CharacterInfo createCharacter() {
-		CharacterInfo character = new CharacterInfo();
-		return character;
-	}
-	@ModelAttribute("myUser")
-	public User putUser() {
-		User user = new User();
-		return user;
-	}
-	
-	@ModelAttribute("battleCharacter")
-	public CharacterInfo createBattleCharacter() {
-		CharacterInfo character = new CharacterInfo();
-		return character;
-	}
-	@ModelAttribute("battleUser")
-	public User putBattleUser() {
-		User user = new User();
-		return user;
-	}
-	
-	@GetMapping
-	public String myPage(HttpSession session, Model model, @ModelAttribute("myCharacter") CharacterInfo character) {
-		
-		User user = (User)session.getAttribute("myUser");
-		int userNum = service.getUserNum(user);
-		user = service.getUserInfo(Integer.toString(userNum));
-		int userCharacterNum = service.getUserCharacterNum(userNum);
-		character = cService.getCharacter(userCharacterNum);
-		log.debug(character);
-		model.addAttribute("myUser", user);
-		model.addAttribute("myCharacter", character);
-		
-		return "userMyPage";
-	}
-	
-	@GetMapping(value = "deleteCharacter")
-	public String deleteCharacter(HttpSession session,@ModelAttribute("myUser") User user) {
-		
-		user = (User)session.getAttribute("myUser");
-		int userNum = service.getUserNum(user);
-		int userCharacterNum = service.getUserCharacterNum(userNum);
-		int count = service.deleteCharacter(userCharacterNum);
-		if(count==1) {
-			log.debug("성공적으로 캐릭터 삭제 완료");
-		}
-		
-		return "myPage";
-	}
-	 
-	@RequestMapping(value="myPageDeleteUser", method = RequestMethod.POST)
-	public String deleteUser(HttpSession session) {
-		
-		User user = (User)session.getAttribute("myUser");
-//			log.debug(user);
-		String userEmail = user.getUserEmail();
-//			log.debug(userEmail);
-//			log.debug("회원정보 읽기 성공");
-		int count = service.deleteUser(userEmail);
-		
-		
-		return "redirect:/resources/main.html";
-	}
+   
+   Logger log = LogManager.getLogger("case3");
+   
+   @Autowired
+   private UserService service;
+   
+   @Autowired
+   private CharacterService cService;
+   
+   @ModelAttribute("myCharacter")
+   public CharacterInfo createCharacter() {
+      CharacterInfo character = new CharacterInfo();
+      return character;
+   }
+   @ModelAttribute("myUser")
+   public User putUser() {
+      User user = new User();
+      return user;
+   }
+   
+   @ModelAttribute("battleCharacter")
+   public CharacterInfo createBattleCharacter() {
+      CharacterInfo character = new CharacterInfo();
+      return character;
+   }
+   @ModelAttribute("battleUser")
+   public User putBattleUser() {
+      User user = new User();
+      return user;
+   }
+   
+   @GetMapping
+   public String myPage(HttpSession session, Model model, @ModelAttribute("myCharacter") CharacterInfo character) {
+      
+      User user = (User)session.getAttribute("myUser");
+      int userNum = service.getUserNum(user);
+      int userCharacterNum = service.getUserCharacterNum(userNum);
+      character = cService.getCharacter(userCharacterNum);
+      log.debug(character);
+      model.addAttribute("myNum", userNum);
+      model.addAttribute("myCharacter", character);
+      
+      return "userMyPage";
+   }
+   
+   @DeleteMapping(value = "character")
+   public void deleteCharacter(HttpSession session,
+         @ModelAttribute("myUser") User user, 
+         HttpServletResponse response,
+         HttpServletRequest request)throws ServletException, IOException {
+      
+      log.debug("@DeleteMapping 호출");
+      log.debug("num : " + request.getParameter("num"));
+      
+      request.setCharacterEncoding("UTF-8");
+       int qone = Integer.parseInt(request.getParameter("num"));
+      
+      user = (User)session.getAttribute("myUser");
+      log.debug("여기로 오면 좋겠어");
+      int userNum = service.getUserNum(user);
+      int userCharacterNum = service.getUserCharacterNum(userNum);
+      log.debug(userCharacterNum);      
+      int count = service.deleteCharacter(userCharacterNum);
+      if(count==1) {
+         log.debug("성공적으로 캐릭터 삭제 완료!");
+      }
+      Gson gson = new Gson();
+       JsonObject jsonObject = new JsonObject();
+       jsonObject.addProperty("num",qone);
+       String find = gson.toJson(jsonObject);
+       response.getWriter().write(find);
+   }
+    
+   @DeleteMapping
+   public String deleteUser(HttpSession session) {
+      
+      User user = (User)session.getAttribute("myUser");
+         log.debug(user);
+      String userEmail = user.getUserEmail();
+         log.debug(userEmail);
+         log.debug("성공적으로 회원 탈퇴 완료!");
+      int count = service.deleteUser(userEmail);
+      
+      
+      return "redirect:/resources/main.html";
+   }
 	
 	@GetMapping(value = "battleuser")
 	public String getUserInfo(HttpSession session, Model model, String battleUserNum, 
@@ -109,4 +130,10 @@ public class MyPageController {
 		model.addAttribute("battleCharacter", character);
 		return "prepBattle";
 	}
+    @PostMapping(value="moveSurvey")
+    public String moveSurvey() {
+         log.debug("성공적인 설문조사 이동!");
+         
+         return "survey/survey1";
+    }
 }
--- MBTI_TeamProject/src/main/webapp/WEB-INF/views/myPage.jsp
+++ MBTI_TeamProject/src/main/webapp/WEB-INF/views/myPage.jsp
@@ -11,57 +11,90 @@
     <meta charset="UTF-8">
     <title>Title</title>
     
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="../js/mypage.js"></script>
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<!-- <script src="../js/mypage.js"></script> -->
 <!-- <script>
-	function popuptest(){
-	    var url = "http://localhost:8080/springmbti/resources/searchBattleUser";
-	    var name = "popup test";
-	    var option = "width = 500, height = 500, top = 100, left = 200, location = no"
-	    window.open(url, name, option);
-	}
+   function popuptest(){
+       var url = "http://localhost:8080/springmbti/resources/searchBattleUser";
+       var name = "popup test";
+       var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+       window.open(url, name, option);
+   }
 </script> -->
 
-
+<script>
+   function myFunc1(){
+ 
+      $.ajax({
+         url: '/springmbti/mypage/character',
+         method: "DELETE",
+         data : {
+           num : 1 
+         },
+         success: function(){
+            alert("성공성공!")
+         },
+         error: function(){
+           
+            alert("무언가 이상해요!")
+         }
+      })
+   }
+ 
+</script>
 
 </head>
 <body>
+
     <div><img src="${ myUser.userProfile }" width="50" height="50">${ myUser.userName }접속중</div>
     <div class="box1" style="font-family:verdana; text-align: left"> 
-		<h2>나만의  MBTI 캐릭터 만들기</h2> <h3 style="font-family:courier">모두 덤벼라!</h3>
-		<h1>MBTI 미니 대전</h1>
-	</div>
+      <h2>나만의  MBTI 캐릭터 만들기</h2> <h3 style="font-family:courier">모두 덤벼라!</h3>
+      <h1>MBTI 미니 대전</h1>
+   </div>
     <!-- 개인 캐릭터 넣기, 없으면 물음표 , 있으면 캐릭터로 이미지 띄우고 -->
     
     <br><br>
     <!-- user email이 primary로 DB에 저장되어 있으면  토큰, 이메일, 닉네임, 프로필 이미지 가져오는데 
-    	db에 없으면 저장이 되는데 , 여기서 확인을 하고  -->
+       db에 없으면 저장이 되는데 , 여기서 확인을 하고  -->
     
     <c:choose>
       <c:when test="${ isExist }">
-      	 <button onClick="location.href='/springmbti/mypage/deleteCharacter'">삭제하기</button>
+          <!-- <button onclick="location.href='/springmbti/mypage/deleteCharacter'">캐릭터 삭제하기</button> -->
+             <button onclick="myFunc1()">내 캐릭터 삭제하기</button>
+            <a href="/springmbti/mypage/searchBattleUser"><button>대결하기</button></a>
+            <form action="/springmbti/mypage/moveSurvey" method="post"
+                  disabled="disabled">
+                <input type="submit" value="설문조사">
+            </form>
+            
       </c:when>
       
       <c:otherwise>
-         <button disabled="disabled">삭제하기</button>
+          <form action="/springmbti/mypage/moveSurvey" method="post">
+                <input type="submit" value="설문조사">
+            </form>
+            <button disabled="disabled">내 캐릭터 삭제하기</button>
+            <button disabled="disabled">대결하기</button>
       </c:otherwise>
       
    </c:choose>
    
-	<form action="/springmbti/character" method="post">
-		<input type="submit" value="캐릭터생성(임시)">
-	</form>
+   
+   
+    <br><br>
 	<form action="/springmbti/mypage" method="get">
 		<input type="submit" value="마이페이지">
 	</form>
-	<!-- 모달넣어야 하고  -->
-	
-    <button>설문조사</button>
-    <a href="/springmbti/mypage/searchBattleUser"><button>대결하기</button></a>
+
+   <!-- 모달넣어야 하고  -->
+   
     <br><br>
     <a href="/springmbti/test">결과 페이지로 이동(test용)</a>
     <br><br>
-	<form action="/springmbti/mypage/myPageDeleteUser" method="post">  
+   <form action="/springmbti/mypage/user">  
     <button>회원탈퇴 (test)</button></form> 
     <!-- 탈퇴 모달 넣기 -->
     
