e350966801f67d6329937b248afb896998c39d58
jaejinlee1008 <ghkfuddks@naver.com>
2023-03-22 18:16:36 +0900

    필터변경_2023/03/22_재진

--- MBTI_TeamProject/src/main/java/team/spring/springmbti/filter/CORSFilter.java
+++ MBTI_TeamProject/src/main/java/team/spring/springmbti/filter/CORSFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CORSFilter implements Filter{
@@ -22,8 +23,10 @@ public class CORSFilter implements Filter{
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-
-		response.setHeader("Access-Control-Allow-Origin", "http://192.168.0.204:8081"); 
+		HttpServletRequest request = (HttpServletRequest) req;
+		String origin = request.getHeader("origin");
+//		response.setHeader("Access-Control-Allow-Origin", "http://e8e4-123-142-252-26.ngrok.io");
+		response.setHeader("Access-Control-Allow-Origin", origin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT"); 
 		response.setHeader("Access-Control-Max-Age", "3600"); 
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
